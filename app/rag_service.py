d36c3b1b715ec011e15f77489daebfccd59317dc
fatemah ahmed <rahmaali.ra9@gmail>
2026-05-11 02:44:19 +0300

    fix import paths

--- app/rag_service.py
+++ app/rag_service.py
@@ -7,7 +7,6 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import Chroma
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains import RetrievalQA
 
 load_dotenv()
